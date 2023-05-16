04c0c2deafbd93b71b4cc49064e0bc915d54b89f
devusSs <flofest98@gmail.com>
2023-05-17 00:42:40 +0200

    added crosshair models

--- server/api/models/models.go
+++ server/api/models/models.go
@@ -1,6 +1,8 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type RegisterUser struct {
 	EMail      string `json:"e_mail"`
@@ -17,4 +19,23 @@ type ReturnUser struct {
 	CreatedAt time.Time `json:"created_at"`
 	EMail     string    `json:"e_mail"`
 	Role      string    `json:"role"`
+}
+
+type Crosshair struct {
+	Added time.Time `json:"added"`
+	Code  string    `json:"code"`
+	Note  string    `json:"note"`
+}
+
+type AddCrosshair struct {
+	Code string `json:"code"`
+	Note string `json:"note"`
+}
+
+type GetMultipleCrosshairs struct {
+	Crosshairs []Crosshair `json:"crosshairs"`
+}
+
+type GetOneCrosshair struct {
+	Crosshair Crosshair `json:"crosshair"`
 }
