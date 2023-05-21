cd722aba7b28f274cecafc51c5d8d399ce48ef11
devusSs <flofest98@gmail.com>
2023-05-21 20:35:35 +0200

    removed invalid header

--- client/src/api/posts.ts
+++ client/src/api/posts.ts
@@ -6,11 +6,10 @@ export function getCrosshairPosts() {
     const axiosInstance = axios.create({
         baseURL: import.meta.env.VITE_BASE_URL,
         withCredentials: true,
-        headers: {"Origin": "http://localhost:5173"}
     })
 
     return axiosInstance
-        .get<successResponse[]>("/api/crosshairs")
+        .get<successResponse[]>("/api/admins/events/user_registered")
         .then((res) => res.status === 200 ? res.data : Promise.reject(res));
 }
 
