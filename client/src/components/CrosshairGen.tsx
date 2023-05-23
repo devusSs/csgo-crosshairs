e66a1e7550a2e190049fe64e68d057bacd3e6881
Lars <lars.siebig.ls@gmail.com>
2023-05-23 23:15:34 +0200

    added auth for routing

--- client/src/components/CrosshairGen.tsx
+++ client/src/components/CrosshairGen.tsx
@@ -3,8 +3,21 @@ import ColorMenu from '../utils/colorMenu'
 import CrosshairStylingMenu from '../utils/crosshairStylingMenu'
 import StyleMenu from '../utils/styleMenu'
 import GenerateSave from '../utils/GenerateSave'
+import { useEffect } from 'react'
+import useAuth  from '../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
 
 function CrosshairGen() {
+ const navigate = useNavigate();
+  const { auth }: any = useAuth();
+ 
+ useEffect((): any => {
+    let isMounted = true;
+    if (auth?.role !== 'user' && auth?.role !== 'admin') {
+      navigate('/login')
+    }
+    return () => (isMounted = false);
+ }, [auth?.role])
   return (
     <div>
         <div className='flex items-center justify-center h-screen'>
