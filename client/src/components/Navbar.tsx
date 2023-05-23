87c7c89157b9b268236254e6b3b74d14e6283bea
Lars <lars.siebig.ls@gmail.com>
2023-05-23 23:49:11 +0200

    added logout + auth to accessible products

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { BiLogIn } from 'react-icons/bi';
+import { BiLogIn , BiLogOut } from 'react-icons/bi';
+import useAuth from '../hooks/useAuth';
+
+import useLogout from '../hooks/useLogout';
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const {auth}: any = useAuth();
+  const logout = useLogout();
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
+  const callLogout = async() => {
+    await logout()
+  }
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -30,16 +39,14 @@ const Navbar: React.FC = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link to="/home" className="text-white font-semibold text-lg">
-              Title
+              dropawp.com
             </Link>
           </div>
           <div className="hidden md:block">
             <div className="ml-4 flex items-center">
-              <Link to="/home" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                Home
-              </Link>
-
-              <div className="relative" ref={dropdownRef}>
+            
+              {auth?.role && (
+                <div className="relative" ref={dropdownRef}>
                 <button onClick={toggleDropdown} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none">
                   Crosshair Generator
                 </button>
@@ -55,19 +62,35 @@ const Navbar: React.FC = () => {
                     </div>
                   </div>
                 )}
+                </div>
+                )}
 
-              <Link to="/demo" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              {auth?.role && (
+                <Link to="/demo" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 Demo Extractor
-              </Link>
+                </Link>
+              )}
+              
+              {!auth?.role && (
+                <Link to="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  <span>
+                    Login
+                  </span>
+                  <BiLogIn className="inline-block ml-1" />
+                </Link>
+              )}
 
-              <Link to="/Login" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              {auth?.role && ( 
+                
+                <button onClick={callLogout} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 <span>
-                  Login
+                  Logout
                 </span>
-                <BiLogIn className="inline-block ml-1" />
-              </Link>
+                <BiLogOut className="inline-block ml-1" />
+              </button>
+              )}
 
-              </div>
+              
             </div>
           </div>
         </div>
