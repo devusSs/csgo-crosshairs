d78ebdbfaa571a49adb05a2ac1e5b60d3306eae7
Lars <lars.siebig.ls@gmail.com>
2023-05-23 23:15:53 +0200

    added auth for routing

--- client/src/components/Demo.tsx
+++ client/src/components/Demo.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useEffect } from 'react'
+import useAuth  from '../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
+
 
 function Demo() {
+  const navigate = useNavigate();
+  const { auth }: any = useAuth();
+ 
+ useEffect((): any => {
+    let isMounted = true;
+    if (auth?.role !== 'user' && auth?.role !== 'admin') {
+      navigate('/login')
+    }
+    return () => (isMounted = false);
+ }, [auth?.role])
   return (
     <div className='text-xl'>Demo</div>
   )
