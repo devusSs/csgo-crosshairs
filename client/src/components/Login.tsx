b96075af76296b29af77c1a2fff8866a44b5a506
Lars <lars.siebig.ls@gmail.com>
2023-05-24 08:49:15 +0200

    started toasts

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -4,16 +4,48 @@ import { Link , useNavigate } from 'react-router-dom';
 import {loginUser} from '../api/requests'
 import { AxiosError } from 'axios';
 import useAuth from '../hooks/useAuth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 
 export default function Login() {
     const navigate = useNavigate();
+    
     const [user, setUser] = useState<User>({
         e_mail: "",
         password: ""
     });
     const {auth, setAuth} : any = useAuth();
     
+    const toastSuccessMessage = () => {
+        toast.success('Login successful!', { position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored", });
+    }
+    const toastErrorMessage = () => {
+        toast.error('Login failed!', { position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",});
+    }  
+
+    const handleToast = () => {
+        if (auth) {
+            toastSuccessMessage()
+        } else {
+            toastErrorMessage()
+        }
+    }
+
     const setNewValue = (e_mail: string , password: string) => 
         setUser(prevState => ({ ...prevState, [e_mail]: password }))
 
@@ -23,15 +55,30 @@ export default function Login() {
         if (response instanceof AxiosError) {
             const errResponse = response?.response?.data as errorResponse;
             setUser({} as User)
+
         } else {
             const sucResponse = response?.data as userSuccessResponse;
             localStorage.setItem('role', sucResponse.data.role)
             setAuth(sucResponse.data)
             navigate('/home')
         }
-    }    
+    }
+    
 
     return (
+        <div>
+            <ToastContainer 
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+            />
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
                 <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
@@ -64,13 +111,14 @@ export default function Login() {
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
-                    <Link to="/reset" className="text-xs text-purple-600 hover:underline">
+                    <Link to="/resetpw" className="text-xs text-purple-600 hover:underline">
                             Forgot Password?
                     </Link>
                     <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                        <button onClick={handleToast} className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
                             Login
                         </button>
+                        
                     </div>
                 </form>
 
@@ -82,6 +130,7 @@ export default function Login() {
                     </Link>
                 </p>
             </div>
+        </div>
         </div>
     );
 }
